6e7d6c47535747878359827ecdf33f2538f76a4c
Santiago Rios <santi_diaz17@hotmail.com>
2020-12-27 22:52:49 -0500

    run build again to solve import errors

--- docs/static/js/17.b1aa4698.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpwolox-challenge"]=this["webpackJsonpwolox-challenge"]||[]).push([[17],{101:function(e,t,o){"use strict";o.r(t);var c=o(1),l=(o(0),function(e){var t=e.total;return Object(c.jsx)("footer",{className:"tech__footer__container",children:Object(c.jsx)("div",{children:Object(c.jsxs)("h4",{children:["Total: ",t]})})})});t.default=l,l.defaultProps={total:0}}}]);
-//# sourceMappingURL=17.b1aa4698.chunk.js.map
